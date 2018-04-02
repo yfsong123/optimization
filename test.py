2c82be8318ee98b232524d4da332ae47bd1e1715
yunfei <songyunfei1986@gmail.com>
2018-04-01 23:37:00 -0400

    first test hello world function

---
+++ test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  1 23:34:39 2018
+
+@author: yunfeisong
+"""
+
+import cvxopt
+import numpy as np
+
+def printTest():
+    print "Hello World"
+    return
+
